0172a4baf0cbf6033b7f5bd8b8cbf23526976fcd
dougsko <dougtko@gmail.com>
2009-09-07 23:49:35 -0400

    wrote mostly everything

--- Rakefile
+++ Rakefile
@@ -5,8 +5,8 @@ begin
   require 'jeweler'
   Jeweler::Tasks.new do |gem|
     gem.name = "apod"
-    gem.summary = %Q{TODO: one-line summary of your gem}
-    gem.description = %Q{TODO: longer description of your gem}
+    gem.summary = "A library to work with NASA's astronomy pictures of the day."
+    gem.description = "A library to work with NASA's astronomy pictures of the day."
     gem.email = "dougtko@gmail.com"
     gem.homepage = "http://github.com/dougsko/apod"
     gem.authors = ["dougsko"]
---
+++ lib/apod.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+#
+# Get links to NASA's Astronomy Pictures of the Day
+#
+
+require 'rubygems'
+require 'open-uri'
+require 'hpricot'
+
+class Apod
+    def initialize
+        @pictures = []
+
+        archive_doc = Hpricot(open("http://antwrp.gsfc.nasa.gov/apod/archivepix.html"))
+        archive_doc.search("/body/b/a").each do |elem|
+            @pictures << Pic.new(elem.innerHTML, "http://antwrp.gsfc.nasa.gov/apod/" + elem["href"])
+        end
+    end
+
+    def pictures
+        @pictures
+    end
+
+    def titles
+        @pictures.collect{ |pic| pic.title }
+    end
+
+    def download(index, save_path)
+        doc = Hpricot(open(@pictures[index].website))
+        link = "http://apod.nasa.gov/" + doc.at("//center/p[2]/a")["href"]
+
+        url = URI.parse(link)
+
+        url.open do |pic|
+            File.open(save_path, "w") do |file|
+                file << pic.read
+            end
+        end
+    end
+end
+
+class Pic
+    attr_reader(:title, :website)
+
+    def initialize(title, website)
+        @title = title
+        @website = website
+    end
+end
